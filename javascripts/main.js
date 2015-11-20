0f4a21221c43a9ed5c7c86e8f8dff90045407ef6
lmq <binglingwy@163.com>
2015-11-20 09:46:02 +0800

    将头部通过js方式渲染

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,39 @@
 $(function(){
-	$('#header').load('/header.html');
+	//$('#header').load('/header.html');
+	var BL = {
+		initPage: function(){
+			$('#header').html(this.getHeader());
+		},
+
+		getHeader: function(){
+			var html = this.template(function(){     
+				/*        
+				<div class="clearfix bg-b cor-w header">
+					<h1 class="f-l fs-14 fw-b ml-20 pt-5"><img src="../images/logo.png" /></h1>
+					<ul class="f-r fs-14 pt-20 pb-20">
+						<li class="f-l"><a class="cor-w mr-20">html</a></li>
+						<li class="f-l"><a class="cor-w mr-20">css</a></li>
+						<li class="f-l"><a class="cor-w  mr-20">javascript</a></li>
+						<li class="f-l"><a class="cor-w mr-20">project</a></li>
+					</ul>
+				</div>    
+				*/
+			});
+			return html;
+		},
+
+		template: function(tmpl){
+			var regEx = new RegExp("/\\*([\\S\\s]*)\\*/", "mg");		
+			tmpl = tmpl + "";		
+			var matches = tmpl.match(regEx) || [];		
+			var result = [];		
+			for (var i = 0; i < matches.length; i++) {			
+				result.push(matches[i].replace(regEx, "$1"));		
+			}		
+			return result.join("");
+			}
+	};
+
+	BL.initPage();
+	
 });
